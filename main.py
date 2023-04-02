1511d295e325cd6a266af7ab9df13bae5ee8ad93
Russell Sabia <rjsabia@gmail.com>
2023-04-02 02:24:18 -0500

    Added and tested Praw reddit connection, worked

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import os
 import openai
+import praw
+
 
 openai.api_key = os.getenv('OPEN_AI_KEY_01')
+REDDIT_CLIENT_ID = os.getenv('REDDIT_CLIENT_ID_01')
+REDDIT_SECRET = os.getenv('REDDIT_SECRET_01')
+
+reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID,
+                     client_secret=REDDIT_SECRET,
+                     user_agent = "sentiment analysis test"
+                     )
+
+for submission in reddit.subreddit("finance").hot(limit=5):
+    print(submission.title)
+
+
+
+
+
+
+
+
