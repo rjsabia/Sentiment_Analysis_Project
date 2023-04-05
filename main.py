c9e615f0162845a9107be8a130a138e4f95d661a
Russell Sabia <rjsabia@gmail.com>
2023-04-04 22:24:32 -0500

    started function to get comments

--- main.py
+++ main.py
@@ -13,8 +13,62 @@ reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID,
                      user_agent = "sentiment analysis test"
                      )
 
-for submission in reddit.subreddit("finance").hot(limit=5):
-    print(submission.title)
+# ---- running some tests on interacting with the reddit api ----
+# for submission in reddit.subreddit("finance").hot(limit=5):
+    # print(submission.title)  # Example for acces to category -->
+                               # reddit.com/r/stocks
+
+# for submission in reddit.subreddit("stocks").hot(limit=10):
+    # print(submission.title)
+
+subreddit_stocks = reddit.subreddit("stocks")
+
+# ---- Example of some data points you can drill into ----
+# subreddit_stocks.title
+# subreddit_stocks.display_name
+# subreddit_stocks.acounts_active
+# etc etc tons of ways to drill in
+# print('\n',subreddit_stocks.accounts_active)
+# print('\n',subreddit_stocks.title)
+# subreddit_stocks.accounts_active
+
+for post in subreddit_stocks.hot(limit=5):
+    print('\n',post.title,'\n','##########################','\n')
+    submission = reddit.submission(post.id)
+    # print top 2 comments per title submission
+    counter = 0
+    for comment in submission.comments:
+        print('COMMENT: ',comment.body,'\n')
+        counter+=1
+        if counter == 2:
+            break
+
+def get_titles_and_comments(subreddit="stocks",limit=6,num_comments=3,skip_first=2):
+    subreddit = reddit.subreddit(subreddit)
+    title_and_comments = {}
+
+    for counter,post in enumerate(subreddit.hot(limit=limit)):
+
+        if counter < skip_first:
+            continue
+
+        counter += (1-skip_first)
+
+        title_and_comments[counter] = ""
+        # this is using the PRAW library again
+        submission = reddit.submission(post.id)
+        title = post.title
+
+        title_and_comments[counter] +='Title: '+title+"\n\n"
+        title_and_comments[counter] += "Comments: \n\n"
+
+
+
+
+
+
+
+
 
 
 
